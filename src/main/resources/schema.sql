a7729701cb3825effe2976ae07ea7de95925f201
Henrique De Moraes <henrique.demoraes@ul.com>
2022-09-18 08:39:35 -0300

    configure H2 database

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+create table user (
+    id            identity        not null,
+    first_name    varchar(255)    not null,
+    last_name     varchar(255)    not null,
+    email         varchar(255)    not null,
+    password      varchar(255)    not null
+);
