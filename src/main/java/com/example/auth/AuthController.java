05f17dec016edbdda4932389aa168678d9c23c48
Henrique De Moraes <henrique.demoraes@ul.com>
2022-09-19 07:05:11 -0300

    add DTO design pattern for register endpoint

--- src/main/java/com/example/auth/AuthController.java
+++ src/main/java/com/example/auth/AuthController.java
@@ -1,5 +1,6 @@
 package com.example.auth;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -11,8 +12,21 @@ public class AuthController {
         this.userRepo = userRepo;
     }
 
+    //record is a way to implements DTO pattern
+    record RegisterRequest(@JsonProperty("first_name") String firstName, @JsonProperty("last_name") String lastName, String email, String password, @JsonProperty("password_confirm") String passwordConfirm){}
+    record RegisterResponse(Long id, @JsonProperty("first_name") String firstName, @JsonProperty("last_name") String lastName, String email){}
+
     @PostMapping(value = "/register")
-    public User register(@RequestBody User user){
-        return userRepo.save(user);
+    public RegisterResponse register(@RequestBody RegisterRequest registerRequest){
+        var user = userRepo.save(
+                //factory method User.of
+                User.of(
+                        registerRequest.firstName(),
+                        registerRequest.lastName(),
+                        registerRequest.email(),
+                        registerRequest.password()
+                )
+        );
+        return new RegisterResponse(user.getId(), user.getFirstName(),user.getLastName(), user.getEmail());
     }
 }
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,4 @@
-create table user (
+create table if not exists user (
     id            identity        not null,
     first_name    varchar(255)    not null,
     last_name     varchar(255)    not null,
